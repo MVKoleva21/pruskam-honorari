97f98f1bc5a5fad58905870b0d7f51cfe6fb4f20
MVKoleva21 <132448406+isstanchev22@users.noreply.github.com>
2023-06-04 21:52:03 +0300

    pisna mi ot vas, starvaite putki

--- da-vdignem-shum/✝️.js
+++ da-vdignem-shum/✝️.js
@@ -1,1 +1,14 @@
-alert("hui");
+var color = document.getElementById("colorBackground");
+        color.addEventListener("click", () => changeBackground());
+        var changeBackground = () => {
+            var randomColor = getRandomColor();
+            document.body.style.background = randomColor;
+        }
+
+        var getRandomColor = () => {
+            var red = Math.floor(Math.random() * 256);
+            var green = Math.floor(Math.random() * 256);
+            var blue = Math.floor(Math.random() * 256);
+            var changeColor = `rgb(${red}, ${green}, ${blue})`;
+            return changeColor;
+        }
